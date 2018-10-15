1779c61d305ec0311cdb19fb2c426e118e60a9dc
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2018-10-15 18:48:41 +0300

    Acrostia redesign

--- Acrostia/js/main.js
+++ Acrostia/js/main.js
@@ -1,15 +1,34 @@
 $(document).ready(function(){
     var containerEl = document.querySelector('.portfolio-container_content');	
 	var mixer =  mixitup(containerEl,{
+        animation: {
+            effects: 'fade translateZ(-100px)',
+            duration: 0
+        },        
 		selectors: {
     		control: '[data-mixitup-control]'
-		}
+		},
+        callbacks: {
+            onMixEnd: function(){
+                $(window).trigger('resize').trigger('scroll');
+            }
+        }            
 	});
 
     $(".portfolio-link").magnificPopup({
         type:"inline",
         midClick: true
     });
+
+    $("a[href^='#']").click(function(e) {
+        e.preventDefault();
+        
+        var position = $($(this).attr("href")).offset().top;
+
+        $("body, html").animate({
+            scrollTop: position
+        } /* speed */ );
+    });   
 
  //   $(".header-title").animated("fadeInUp", "fadeOut");
 
@@ -19,7 +38,7 @@ $(document).ready(function(){
 
     $(".team-container_item").animated("fadeIn", "fadeOut");
 
-    $(".portfolio-container_content_item").animated("fadeIn", "fadeOut");
+    $(".portfolio-container_content_item").animated("fadeIn");
 
     $(".pricing-container_item").animated("flipInY", "fadeOut");
 
