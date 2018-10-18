54f6dac5df52c62a85f8a63df44fe52c5d666f4b
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2018-10-18 18:57:12 +0300

    Tanatos owl-carousel mobile fix

--- Tanatos/js/common.js
+++ Tanatos/js/common.js
@@ -36,6 +36,12 @@ $(document).ready(function(){
         }
     });
 
+   if ($(window).width() <= 768) {
+    $('.owl-carousel').owlCarousel({
+      mouseDrag: false
+    });
+  }
+
 
     $(".header_content").animated("fadeIn", "fadeOut");
 
