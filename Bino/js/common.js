b877be250cea455f236c92a9c770a51059ea4603
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2020-04-11 23:07:55 +0300

    animation fixed

--- Bino/js/common.js
+++ Bino/js/common.js
@@ -30,28 +30,30 @@ $(document).ready(function(){
   
     });
 
-    $(".study-description").animated("fadeInLeft");
+    if ($(window).width() > 576) {
+        $(".study-description").animated("fadeInLeft");
 
-   $(".study-image").animated("fadeInRight");    
+        $(".study-image").animated("fadeInRight");    
 
-    $(".section-header").animated("fadeIn");
+        $(".section-header").animated("fadeIn");
 
-    $(".header-description").animated("fadeIn");
+        $(".header-description").animated("fadeIn");
 
-    $(".whoweare-container").animated("fadeIn");
+        $(".whoweare-container").animated("fadeIn");
 
-    $(".portfolio-item").animated("fadeIn");
+        $(".portfolio-item").animated("fadeIn");
 
-    $(".stats-icon-container").animated("fadeIn");
+        $(".stats-icon-container").animated("fadeIn");
 
-    $(".pricing-wrapper").animated("fadeIn");
+        $(".pricing-wrapper").animated("fadeIn");
 
-    $(".team-container_item").animated("fadeIn");
+        $(".team-container_item").animated("fadeIn");
 
-    $(".client-item").animated("fadeIn");
+        $(".client-item").animated("fadeIn");
 
-    $(".blog-post").animated("fadeIn");
+        $(".blog-post").animated("fadeIn");
 
-    $(".contact-container").animated("fadeIn");    
+        $(".contact-container").animated("fadeIn");
+    }
 
 })	
